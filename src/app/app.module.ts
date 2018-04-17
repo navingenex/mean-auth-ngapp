3f490d3da46da9acd626a4c08afb4aac29b90221
navingenex <navingenex@gmail.com>
2018-04-17 17:26:46 +0530

    login, logout, authGuard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,13 +14,18 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 
+import{ValidateService} from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+
+
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent ,canActivate:[AuthGuard]},
+  { path: 'dashboard', component: DashboardComponent ,canActivate:[AuthGuard]},
 ]
 @NgModule({
   declarations: [
@@ -30,9 +39,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    ValidateService,
+    AuthService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from './../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from './../../services/auth.service';
+import{Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  constructor(private validateService: ValidateService,
+    private flashMessage: FlashMessagesService,
+    private authService: AuthService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
+  }
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+    //required field
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show('Please fill all details', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show('Please fill valid email ', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+
+    //register users
+    this.authService.registerUser(user)
+    .subscribe(data=>{
+      if(data.success){
+        this.flashMessage.show('You are now registerd and can login', { cssClass: 'alert-success', timeout: 3000 });
+        this.router.navigate(['/login']);
+      }else{
+        {
+          this.flashMessage.show('Something went wrong', { cssClass: 'alert-danger', timeout: 3000 });
+          this.router.navigate(['/register']);
+        }
+      }
+    })
   }
 
 }
